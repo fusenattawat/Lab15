5770fd3c9190f93158b2a326a2d99f5aa250886b
fusenattawat <141806694+fusenattawat@users.noreply.github.com>
2024-02-02 13:50:35 +0700

    lab15_4.cpp

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -6,14 +6,28 @@ using namespace std;
 void shuffle(int &,int &,int &,int &);
 
 int main(){
-	int a = 50, b = 100, c = 500, d = 1000;
-	
-	srand(time(0));	
-	
-	for(int i = 0;i < 10;i++){
-	    shuffle(a,b,c,d);
-	    cout << a << " " << b << " " << c << " " << d << "\n";
-	}
-	
-	return 0;
+ int a = 50, b = 100, c = 500, d = 1000;
+ 
+ srand(time(0)); 
+ 
+ for(int i = 0;i < 10;i++){
+     shuffle(a,b,c,d);
+     cout << a << " " << b << " " << c << " " << d << "\n";
+ }
+ 
+ return 0;
+}
+
+void shuffle(int &a, int &b, int &c, int &d){
+    int A[] = {a, b, c, d};
+ for(int i = 0; i < 4; i++){
+  int j = rand() % 4;
+  int temp = A[i];
+  A[i] = A[j];
+  A[j] = temp;
+ }
+ a = A[0];
+ b = A[1];
+ c = A[2];
+ d = A[3];
 }
